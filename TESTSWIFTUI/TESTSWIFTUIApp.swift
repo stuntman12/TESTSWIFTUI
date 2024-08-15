52e22596224b271e439d05885d218b313db59f26
Daniil <developer_ivanov.d.a@icloud.com>
2024-08-15 12:46:41 +0300

    Initial Commit

---
+++ TESTSWIFTUI/TESTSWIFTUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTSWIFTUIApp.swift
+//  TESTSWIFTUI
+//
+//  Created by Даниил on 15.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TESTSWIFTUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
